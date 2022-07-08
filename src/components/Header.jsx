71ac20235a3fd78157eaf66ebf2f4dab32d01d7f
Bernardo Rodrigues <bernardorg99@hotmail.com>
2022-07-08 05:45:02 -0300

    Estilizei o navbar no topo do card

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,48 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 function Header() {
   return (
     <nav>
-      <Link to="/">Profile</Link>
-      <Link to="/educacao">Educação</Link>
-      <Link to="/experiencia">Experiência</Link>
-      <Link to="/habilidades">Habilidades</Link>
+      <NavLink to="/">
+        <img
+         src="https://raw.githubusercontent.com/brodrigues0ll/portifolio/ba3b7b4024d26aa3ffbc7c95b75ab5bad29c4232/src/images/navbar/edit-profile.svg"
+         alt="profle-icon"
+         height="40"
+        />
+          Perfil
+      </NavLink>
+
+
+      <NavLink to="/educacao">
+        <img
+        src="https://raw.githubusercontent.com/brodrigues0ll/portifolio/ba3b7b4024d26aa3ffbc7c95b75ab5bad29c4232/src/images/navbar/graduation-cap.svg"
+        alt="graduation-cap-icon"
+        height="40"
+      />
+        Educação
+      </NavLink>
+
+
+      <NavLink to="/experiencia">
+      <img
+        src="https://raw.githubusercontent.com/brodrigues0ll/portifolio/ba3b7b4024d26aa3ffbc7c95b75ab5bad29c4232/src/images/navbar/necktie.svg"
+        alt="necktie-icon"
+        height="40"
+      />
+        Experiência
+      </NavLink>
+
+
+      <NavLink to="/habilidades">
+      <img
+        src="https://raw.githubusercontent.com/brodrigues0ll/portifolio/ba3b7b4024d26aa3ffbc7c95b75ab5bad29c4232/src/images/navbar/jet-with-arms.svg"
+        alt="jet-with-arms-icon"
+        height="40"
+        className="jet-icon"
+      />
+        Habilidades
+      </NavLink>
     </nav>
   )
 }
--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -6,4 +6,4 @@ function Perfil() {
   )
 }
 
-export default Perfil;
+export default Perfil
