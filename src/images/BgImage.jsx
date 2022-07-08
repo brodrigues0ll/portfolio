ba3b7b4024d26aa3ffbc7c95b75ab5bad29c4232
Bernardo Rodrigues <bernardorg99@hotmail.com>
2022-07-08 01:17:15 -0300

    Criei o card principal onde a aplicação vai rodar

---
+++ src/images/BgImage.jsx
@@ -0,0 +1,10 @@
+
+function BgImage() {
+  return (
+    <div>
+      <img src="https://github.com/brodrigues0ll/portifolio/blob/main/src/images/bg-image.png?raw=true" alt="background" />
+    </div>
+  )
+}
+
+export default BgImage
